6a6915231ec363c83792ac04cc29cc8a1db4bd9f
leonardo9245 <leonardo92reis@gmail.com>
2023-06-07 18:30:38 -0300

    feat: configuring header

---
+++ capputeeno/src/components/cart-control.tsx
@@ -0,0 +1,34 @@
+import { useLocalStorage } from '@/hooks/useLocalStorage';
+import { CartIcon } from './cart-icon';
+import { styled } from 'styled-components';
+
+const CartCount = styled.span`
+  width: 20px;
+  height: 20px;
+  border-radius: 100%;
+  font-size: 12px;
+
+  background-color: var(--delete-color);
+  color: white;
+
+  position: absolute;
+  right: -10px;
+  top: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+export function CartControl() {
+  const { value } = useLocalStorage('cart-items');
+  return (
+    <Container>
+      <CartIcon />
+      {value.length && <CartCount>{value.length}</CartCount>}
+    </Container>
+  );
+}
